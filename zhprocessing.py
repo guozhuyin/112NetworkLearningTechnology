86a871db5377eb9910598ec5cd3f68a290e26a79
guozhuyin <632939ion@gmail.com>
2023-12-21 23:39:00 +0800

    231221

--- zhprocessing.py
+++ zhprocessing.py
@@ -1,8 +1,10 @@
 import re
 
-strA = ""
-# 如果strA有中文將中文取出
-listA = re.findall(r'[\u4e00-\u9fa5]', strA)
+strA = "我是一個中文句子，我有一些中文標點符號，例如，。；：、（）「」。"
+# 如果strA有中文及中文標點符號，則取出
+strA = re.sub(r'[^\u4e00-\u9fa5，。；：、（）「」]', "", strA)
+# 將strA轉成list
+listA = list(strA)
 
 for i in range(len(listA)):
     for j in range(i+1, len(listA)):
